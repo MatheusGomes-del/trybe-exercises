16ddadf92fee4203118956dc2db5d733c3fe10ee
matheusGomes-del <matheus.gomes.santos@outlook.com>
2022-02-02 18:32:09 -0300

    exercicios fixacao

---
+++ Fundamentos/Bloco04-JavaScript-Logica-Programacao/exercicios/fixacao01/script.js
@@ -0,0 +1,13 @@
+const myName = 'Matheus'
+const birthCity = 'João Pessoa'
+let birthYear = 2003 
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+
+birthYear = 2030
+console.log(birthYear)
+
+birthCity = 'Natal'
+console.log(birthCity)
---
+++ Fundamentos/Bloco04-JavaScript-Logica-Programacao/exercicios/fixacao02/script.js
@@ -0,0 +1,12 @@
+let patientId = 50;
+patientId = '50'
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientAge)
+console.log(typeof patientInfo)
+console.log(typeof patientId)
