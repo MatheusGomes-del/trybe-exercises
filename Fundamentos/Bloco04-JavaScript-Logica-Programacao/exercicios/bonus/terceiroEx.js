2cfa462b336b9f24892be347728de64108e6caf9
matheusGomes-del <matheus.gomes.santos@outlook.com>
2022-02-06 15:47:34 -0300

    retira variavel

--- Fundamentos/Bloco04-JavaScript-Logica-Programacao/exercicios/bonus/terceiroEx.js
+++ Fundamentos/Bloco04-JavaScript-Logica-Programacao/exercicios/bonus/terceiroEx.js
@@ -1,6 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numberMulti = []
-let count = 0
 
 for (let index = 0; index < numbers.length; index += 1) {
     numberMulti.push(numbers[index] * numbers[index + 1] )
