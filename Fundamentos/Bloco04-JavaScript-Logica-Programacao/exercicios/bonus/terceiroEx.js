35acc242e5d1756766639a8455764e06806fa76e
matheusGomes-del <matheus.gomes.santos@outlook.com>
2022-02-06 16:52:47 -0300

    ajustando codigo

--- Fundamentos/Bloco04-JavaScript-Logica-Programacao/exercicios/bonus/terceiroEx.js
+++ Fundamentos/Bloco04-JavaScript-Logica-Programacao/exercicios/bonus/terceiroEx.js
@@ -2,9 +2,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numberMulti = []
 
 for (let index = 0; index < numbers.length; index += 1) {
-    numberMulti.push(numbers[index] * numbers[index + 1] )
     if( index !== numbers.length - 1){
-        console.log()
+      numberMulti.push(numbers[index] * numbers[index + 1])
     }else{
         numberMulti.push(numbers[index] * 2  )
     }
